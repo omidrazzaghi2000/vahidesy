f863d2914ec17a1aa8477469f35991daa3f4422e
omiddd <omiddd@NOR>
2024-05-12 20:52:37 +0330

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { Link, Events, scrollSpy } from "react-scroll";
+import { CgMenuRight } from "react-icons/cg";
+import { UserData } from "../data/UserData";
+import logo from "../Assets/images/logor.png";
+
+const Header = () => {
+  const [isScrolling, setisScrolling] = useState(false);
+  const [activeSection, setActiveSection] = useState(null);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const { resumeUrl } = UserData;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const isCurrentScrolled = scrollTop > 0;
+      setisScrolling(isCurrentScrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    Events.scrollEvent.register("begin", function (to) {
+      setActiveSection(to);
+    });
+
+    scrollSpy.update();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      Events.scrollEvent.remove("begin");
+    };
+  }, []);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
+  return (
+    <header
+      className={`fixed top-0 z-50 flex w-full items-center justify-between text-base transition-all sm:px-4 lg:px-28 lg:pt-2
+      ${isScrolling ? "sticky" : ""}`}
+    >
+      <div className="cursor-none">
+        <img
+          className="h-[80px] w-[90px] bg-cover bg-no-repeat"
+          src={logo}
+          alt="logo"
+        />
+      </div>
+      
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/data/UserData.jsx
@@ -0,0 +1,35 @@
+export const UserData = {
+  name: "Vahid & Ali Akbar",
+  about : "We are so googooli and attractive please join us",
+  resumeUrl : "https://drive.google.com/file/d/1LSVLVmJA_3fhWtZPc_d_pHnkXhL4s-Oc/view",
+  FooterLink : "https://www.linkedin.com/in/rahulvijay81/",
+  socialMedia: [
+    
+    {
+      socialMediaName: "linkedin",
+      url: "https://www.linkedin.com/in/omid-r-70a131b3/",
+      icon: "FaLinkedinIn",
+    },
+    {
+      socialMediaName: "twitter",
+      url: "https://twitter.com/AttendSlayer",
+      icon: "AiOutlineTwitter",
+    },
+    {
+      socialMediaName: "instagram",
+      url: "https://www.instagram.com/vahidesy/",
+      icon: "AiFillInstagram",
+    },
+  ],
+  typewriterOptions: {
+    strings: [
+      "Beautiful Boys",
+      "Conciseness Guys",
+      "Neuroscience Doctors",
+
+    ],
+    autoStart: true,
+    loop: true,
+    deleteSpeed: 30,
+  },
+};
